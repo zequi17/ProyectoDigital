fa578495e533901b17400c07898ef7a4428d8db3
Ezequiel <pkclear_17@hotmail.com>
2019-03-10 02:37:40 -0300

    Migracion de login, registracion y Home

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,42 @@
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>SportsWear</title>
+    <link rel="stylesheet" href="css registracion/register-login.css">
+    <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/register-login.css">
+  </head>
+  <body>
+          <img src='../Fondo.jpg' alt="fondo">
+    <div class="padre">
+      <a href="" class="titulo">SportsWear</a>
+      <div class="formularioLogin" style="padding-left: 193px;">
+      <p class="is">Iniciar sesión</p>
+      <form class="login" action="login.php" method="POST">
+        <p>
+          <input id="email" type="text" name="email" value="" placeholder="Correo Electronico">
+        </p>
+        <div class="pass">
+        <p>
+          <input id="password" type="password" name="password" value="" placeholder="Contraseña">
+        </p>
+        </div>
+        <div class="boton">
+        <button type="submit" name="button">Iniciar sesión</button>
+        </div>
+      </form>
+
+      <p style="color: white;">¿No estas registrado? <a href="register.login.php" style="color: white;">Registrate</a></p>
+        </form><br><br><br><br>
+      </div>
+      <footer>
+        <ul>
+          <li><a href="https://www.facebook.com"><i class="fab fa-facebook"></i></a></li>
+          <li><a href="https://www.twitter.com"><i class="fab fa-twitter-square"></i></a></li>
+          <li><a href="https://www.instagram.com"><i class="fab fa-instagram"></i></a></li>
+        </ul>
+      </footer>
+    </div>
+  </body>
+</html>
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>SportsWear</title>
+    <link rel="stylesheet" href="css registracion/register-login.css">
+    <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/register-login.css">
+  </head>
+  <body>
+          <img src="../Fondo.jpg" alt="fondo">
+    <div class="padre">
+      <a href="" class="titulo" style="padding-left: 53px;">SportsWear</a>
+      <ul style="color:red;background-color:black;">
+        <div class="formularioRegis" style="padding-left: 193px;">
+          <!-- REGISTRACION -->
+      <p class="regis">Registrate</p>
+        <form class="register" action="register.login.php" method="POST" enctype="multipart/form-data">
+          <p>
+            <input id="usuario" type="text" name="usuario" value="" placeholder="Nombre">
+          </p>
+          <div class="pass">
+            <input type="file" name="avatar" value="">
+          </div>
+          <div class="pass">
+          <p>
+            <input id="email" type="text" name="email" value="" placeholder="Mi correo electronico">
+          </p>
+          </div>
+          <div class="pass">
+          <p>
+            <input id="password" type="text" name="password" value="" placeholder="Confirmar correo electronico">
+          </p>
+          </div>
+          <div class="pass">
+          <p>
+            <input id="passwordConfirm" type="password" name="passwordConfirm" value="" placeholder="Mi contraseña">
+          </p>
+          </div>
+          <div class="boton">
+          <button type="submit" name="button">Crear perfil</button>
+          </div>
+          <p style="color: white;">
+            ¿Ya tienes una cuenta? <a href="login.php" style="color: white;">Iniciar sesión</a>
+          </p>
+        </form><br><br><br><br>
+      </div>
+      <footer>
+        <ul>
+          <li><a href="https://www.facebook.com"><i class="fab fa-facebook"></i></a></li>
+          <li><a href="https://www.twitter.com"><i class="fab fa-twitter-square"></i></a></li>
+          <li><a href="https://www.instagram.com"><i class="fab fa-instagram"></i></a></li>
+        </ul>
+      </footer>
+    </div>
+  </body>
+</html>
